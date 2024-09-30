3480bf22ad33db4779565fd8b118a0a148e3674c
kaya70875 <shortsforturkey@gmail.com>
2024-09-30 18:14:41 +0300

    create html structure for form section

--- multi-step-form/src/components/FormInfoSection.tsx
+++ multi-step-form/src/components/FormInfoSection.tsx
@@ -13,7 +13,25 @@ export default function FormInfoSection({formHeader , formInfo} : FormInfoSectio
         <p>{formInfo}</p>
       </header>
 
-      <section className='form'></section>
+      <section className='form'>
+        <form action="submit">
+          <div className="input-wrapper">
+            <label htmlFor="name">Name</label>
+            <input type="text" name='name' placeholder='e.g. Stephen King' />
+          </div>
+
+          <div className="input-wrapper">
+            <label htmlFor="email">Email Adress</label>
+            <input type="text" name='email' placeholder='e.g. Stephenking@lorem.com' />
+          </div>
+
+          <div className="input-wrapper">
+            <label htmlFor="phone">Phone Number</label>
+            <input type="text" name='phone' placeholder='e.g. +1 234 567 890' />
+          </div>
+          
+        </form>
+      </section>
     </div>
   )
 }
