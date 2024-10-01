0c91f76e6303b379730a58422c6c00761f464b0e
kaya70875 <shortsforturkey@gmail.com>
2024-10-01 19:29:13 +0300

    create formProvider

--- multi-step-form/src/components/buttons/PreviousButton.tsx
+++ multi-step-form/src/components/buttons/PreviousButton.tsx
@@ -2,7 +2,9 @@ export default function PreviousButton() {
   return (
     <div className="button-wrapper">
         <button type='button'
-         className='secondary-btn'>Previous</button>
+         className='secondary-btn' onClick={() => {
+          window.history.back();
+         }}>Previous</button>
     </div>
   )
 }
---
+++ multi-step-form/src/context/FormContext.tsx
@@ -0,0 +1,39 @@
+import { createContext, useContext, useState } from "react";
+
+interface FormContextType {
+    formData: FormData;
+    setFormData: (data: FormData) => void;
+}
+
+export interface FormData {
+    plan: string;
+    billing: string;
+    addOns: string[];
+}
+
+const FormContext = createContext<FormContextType | null>(null);
+
+const FormProvider: React.FC<{ children: React.ReactNode }> = ({ children }) => {
+
+    const [formData, setFormData] = useState<FormData>({
+        plan : '',
+        billing : '',
+        addOns : [],
+    });
+
+    return (
+        <FormContext.Provider value={{ formData, setFormData }}>
+            {children}
+        </FormContext.Provider>
+    )
+}
+
+export const useFormContext = () => {
+    const context = useContext(FormContext);
+    if (!context) {
+        throw new Error('useFormContext must be used within a FormProvider');
+    }
+    return context;
+}
+
+export default FormProvider;
