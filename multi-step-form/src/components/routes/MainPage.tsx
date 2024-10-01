3430e7b120c47d70972f49e3130d520cc3b86172
kaya70875 <shortsforturkey@gmail.com>
2024-10-01 14:04:43 +0300

    adding formState

--- multi-step-form/src/components/routes/MainPage.tsx
+++ multi-step-form/src/components/routes/MainPage.tsx
@@ -5,6 +5,19 @@ import SubmitButton from "../buttons/FormButton";
 
 export default function MainPage() {
   const [activeStep , setActiveStep] = useState<number>(1);
+
+  const [formData, setFormData] = useState({
+    name: "",
+    email: "",
+    phone: "",
+  });
+
+  const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    setFormData((prevData) => ({
+      ...prevData,
+      [e.target.name]: e.target.value,
+    }));
+  };
   return (
     <>
       <FormStepsSection activeStep={activeStep}/>
@@ -16,17 +29,17 @@ export default function MainPage() {
         <form>
           <div className="input-wrapper">
             <label htmlFor="name">Name</label>
-            <input type="text" name='name' placeholder='e.g. Stephen King' />
+            <input type="text" name='name' placeholder='e.g. Stephen King' onChange={handleChange}/>
           </div>
 
           <div className="input-wrapper">
             <label htmlFor="email">Email Adress</label>
-            <input type="text" name='email' placeholder='e.g. Stephenking@lorem.com' />
+            <input type="text" name='email' placeholder='e.g. Stephenking@lorem.com' onChange={handleChange}/>
           </div>
 
           <div className="input-wrapper">
             <label htmlFor="phone">Phone Number</label>
-            <input type="text" name='phone' placeholder='e.g. +1 234 567 890' />
+            <input type="text" name='phone' placeholder='e.g. +1 234 567 890' onChange={handleChange}/>
           </div>
           
           <SubmitButton setActiveStep={setActiveStep}/>
