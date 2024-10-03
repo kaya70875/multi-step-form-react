13ca7875ae969e8a49b83776a5d2c11dcc21a145
kaya70875 <shortsforturkey@gmail.com>
2024-10-03 14:48:35 +0300

    setting navigate page

--- multi-step-form/src/components/buttons/FormButton.tsx
+++ multi-step-form/src/components/buttons/FormButton.tsx
@@ -1,3 +1,4 @@
+import { useNavigate } from 'react-router-dom';
 import './_FormButton.scss'
 
 interface SubmitButtonProps {
@@ -5,9 +6,16 @@ interface SubmitButtonProps {
 }
 
 export default function SubmitButton({isLastStep = false} : SubmitButtonProps) {
+  const navigate = useNavigate();
+
+  const handleClick = () => {
+    if(isLastStep) {
+      navigate('/thank-you');
+    }
+  }
   return (
     <div className='button-wrapper'>
-      <button type='submit' className='primary-btn'>{isLastStep ? 'Confirm' : 'Next Step'}</button>
+      <button type='submit' onClick={handleClick} className='primary-btn'>{isLastStep ? 'Confirm' : 'Next Step'}</button>
     </div>
   )
 }
