bf381ca97a97d27f93a2ac4d252382db51383a7f
kaya70875 <shortsforturkey@gmail.com>
2024-10-03 19:20:01 +0300

    update app

--- multi-step-form/src/App.tsx
+++ multi-step-form/src/App.tsx
@@ -1,4 +1,4 @@
-import { Route, Routes, BrowserRouter as Router } from "react-router-dom";
+import { Route, Routes, HashRouter as Router } from "react-router-dom";
 import "./App.css";
 import "./index.css";
 import MainPage from "./components/routes/MainPage";
