8167b722f350c44cdaaf5eb47c81ca8da0696f8c
kaya70875 <shortsforturkey@gmail.com>
2024-10-03 18:45:35 +0300

    complete fully responsive design

--- multi-step-form/src/components/routes/ThankYou.tsx
+++ multi-step-form/src/components/routes/ThankYou.tsx
@@ -1,18 +1,21 @@
 import FormStepsSection from "../FormStepsSection";
 import '../forms/_ThankYou.scss';
 import completeIdle from '../../assets/images/icon-thank-you.svg';
+import FormInfoSection from "../FormInfoSection";
 
 export default function ThankYou() {
   return (
     <>
         <FormStepsSection currentStep={4} />
-        <div className="thank-wrapper">
-            <img src={completeIdle} />
-            <header className="thank-header">
-                <h1>Thank you!</h1>
-                <p>Thanks for confirming your subscription! We hope you have fun using our platform. If you ever need support, please feel free to email us at support@loremgaming.com.</p>
-            </header>
-        </div>
+        <FormInfoSection formHeader="" formInfo="">
+          <form className="thank-wrapper">
+              <img src={completeIdle} />
+              <header className="thank-header">
+                  <h1>Thank you!</h1>
+                  <p>Thanks for confirming your subscription! We hope you have fun using our platform. If you ever need support, please feel free to email us at support@loremgaming.com.</p>
+              </header>
+          </form>
+        </FormInfoSection>
     </>
   )
 }
